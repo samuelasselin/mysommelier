b2b7201c478fd21c93b9abd4e505829c445047eb
samuelasselin <c.samuelasselin@hotmail.com>
2024-05-29 20:04:02 -0400

    Rdy for deploy V.1

--- locales/i18n.ts
+++ locales/i18n.ts
@@ -4,8 +4,8 @@ import { initReactI18next } from 'react-i18next';
 i18n
     .use(initReactI18next)
     .init({
-        lng: 'en',
-        fallbackLng: 'en',
+        lng: 'fr',
+        fallbackLng: 'fr',
         debug: true,
         resources: {
             fr: {
--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   i18n: {
-    locales: ['en', 'fr'],
-    defaultLocale: 'en',
+    locales: ['fr', 'en'],
+    defaultLocale: 'fr',
   },
   reactStrictMode: true,
 };
